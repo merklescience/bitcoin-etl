23ea4f61bc26475ffa94d128ca1b6e38e1395c0d
Akshay Gupta <akshay@merklescience.com>
2021-08-26 17:33:42 +0530

    chunking publish to pubsub

--- blockchainetl/jobs/exporters/google_pubsub_item_exporter.py
+++ blockchainetl/jobs/exporters/google_pubsub_item_exporter.py
@@ -45,16 +45,20 @@ class GooglePubSubItemExporter:
         pass
 
     def export_items(self, items):
-        try:
-            self._export_items_with_timeout(items)
-        except timeout_decorator.TimeoutError as e:
-            # A bug in PubSub publisher that makes it stalled after running for some time.
-            # Exception in thread Thread-CommitBatchPublisher:
-            # details = "channel is in state TRANSIENT_FAILURE"
-            # https://stackoverflow.com/questions/55552606/how-can-one-catch-exceptions-in-python-pubsub-subscriber-that-are-happening-in-i?noredirect=1#comment97849067_55552606
-            logging.info('Recreating Pub/Sub publisher.')
-            self.publisher = self.create_publisher()
-            raise e
+        tot_steps = (len(items) // 1000) + 1
+        logging.info('Total publish loop steps', tot_steps)
+        for i in range(0, len(items), 1000):
+            logging.info('Current Loop Iteration', i + 1, 'out of', tot_steps)
+            try:
+                self._export_items_with_timeout(items)
+            except timeout_decorator.TimeoutError as e:
+                # A bug in PubSub publisher that makes it stalled after running for some time.
+                # Exception in thread Thread-CommitBatchPublisher:
+                # details = "channel is in state TRANSIENT_FAILURE"
+                # https://stackoverflow.com/questions/55552606/how-can-one-catch-exceptions-in-python-pubsub-subscriber-that-are-happening-in-i?noredirect=1#comment97849067_55552606
+                logging.info('Recreating Pub/Sub publisher.')
+                self.publisher = self.create_publisher()
+                raise e
 
     @timeout_decorator.timeout(300)
     def _export_items_with_timeout(self, items):
