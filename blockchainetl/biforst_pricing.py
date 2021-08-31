5a47476e269f47f2420e8be2579ded58b806a280
Akshay Gupta <akshay@merklescience.com>
2021-08-31 15:08:35 +0530

    bifrost params changed

--- blockchainetl/biforst_pricing.py
+++ blockchainetl/biforst_pricing.py
@@ -48,8 +48,8 @@ def _make_request(
     session.mount('http://', adapter)
     retries.BACKOFF_MAX = 600
     params = {
-        "fsyms": from_currency_code,
-        "tsyms": to_currency_code,
+        "from_currencies": from_currency_code,
+        "to_currencies": to_currency_code,
         "access_token": access_token,
         "execution_date": execution_date
     }
