5ae1fc0b719084ab2d2e6c396ee153bb0b330f1d
saurabhdaga-merkle <saurabh@merklescience.com>
2023-01-31 10:49:04 +0530

    remove weight

--- bitcoinetl/mappers/transaction_mapper.py
+++ bitcoinetl/mappers/transaction_mapper.py
@@ -70,7 +70,6 @@ class BtcTransactionMapper(object):
         transaction.value_balance = bitcoin_to_satoshi(json_dict.get('valueBalance'))
 
         # New fields
-        transaction.weight = json_dict.get('weight')
         transaction.output_addresses = self.get_output_addresses(transaction)
         return transaction
 
@@ -98,7 +97,6 @@ class BtcTransactionMapper(object):
             'input_value': transaction.calculate_input_value(),
             'output_value': transaction.calculate_output_value(),
             'fee': transaction.calculate_fee(),
-            'weight': transaction.weight,
             'output_addresses': transaction.output_addresses
         }
         return result
@@ -114,7 +112,6 @@ class BtcTransactionMapper(object):
         transaction.block_hash = dict.get('block_hash')
         transaction.block_timestamp = dict.get('block_timestamp')
         transaction.is_coinbase = dict.get('is_coinbase')
-        transaction.weight = dict.get('weight')
         transaction.output_addresses = dict.get('output_addresses')
         transaction.input_addresses = dict.get('input_addresses')
         transaction.input_count = dict.get('input_count')
